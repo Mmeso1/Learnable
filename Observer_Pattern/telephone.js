124c16975cf621c96f24f2e738fe27aed0cdcbac
Mmeso1 <anisiubammesoma8@gmail.com>
2025-02-15 09:55:32 +0000

    still working on it, but i think i have gotten a hange on how to make the 2 different observer objects print unique messages

--- Observer_Pattern/telephone.js
+++ Observer_Pattern/telephone.js
@@ -1,21 +1,71 @@
 class Telephone {
   constructor() {
     this.observers = [];
+    this.telephoneNumbers = new Set();
   }
-
-  telephoneNumbers = new Set();
 
   AddPhoneNumber(phone_number) {
     this.telephoneNumbers.add(phone_number);
+    console.log("Phone number added ", phone_number);
   }
 
   RemovePhoneNumber(phone_number) {
     if (this.telephoneNumbers.has(phone_number)) {
       console.log("Phone number to del ", phone_number);
       this.telephoneNumbers.delete(phone_number);
+    } else {
+      console.log("Phone number not found");
     }
-    console.log("Phone number not found");
   }
 
-  DialPhoneNumber(phone_number) {}
+  DialPhoneNumber(phone_number) {
+    if (this.telephoneNumbers.has(phone_number)) {
+      this.notifyObservers(phone_number);
+    } else {
+      console.log("Phone number not found");
+    }
+  }
+
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  removeObserver(observer) {
+    this.observers = this.observers.filter((obs) => obs !== observer);
+  }
+
+  notifyObservers(phone_number) {
+    this.observers.forEach((observer) => {
+      observer.notify(phone_number);
+    });
+  }
 }
+
+class Observer {
+  constructor(action) {
+    //passing in the action or function i want to be done
+    this.action = action; //storing the action in the observer
+  }
+
+  notify(phone_number) {
+    // console.log("Observer notfied that there is dialing in progress");
+    this.action(phone_number); //calling the action when notify is called
+  }
+}
+
+var telephone = new Telephone();
+telephone.AddPhoneNumber("2347023232");
+telephone.AddPhoneNumber("2347023233");
+
+var Observer1 = new Observer((phone_number) => {
+  console.log(phone_number);
+});
+
+var Observer2 = new Observer((phone_number) => {
+  console.log(`Now Dialling ${phone_number}`);
+});
+
+telephone.addObserver(Observer1);
+telephone.addObserver(Observer2);
+telephone.notifyObservers();
+telephone.DialPhoneNumber("2347023232");
