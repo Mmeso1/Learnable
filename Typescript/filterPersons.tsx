8dbfcd74da53c73b2e76ca9dbf3be3404b8ff5fc
Mmeso1 <anisiubammesoma8@gmail.com>
2025-02-19 12:02:20 +0000

    I thought I was done initially, LOL. I went through the requirements againa nd saw I still had a lot do like the partial objects and return types based on personType. I believe I am done. WIll test with other parameters after this push

--- Typescript/filterPersons.tsx
+++ Typescript/filterPersons.tsx
@@ -38,8 +38,18 @@ export function logPerson(person: Person) {
 
 export function filterPersons(
   persons: Person[],
+  personType: "user",
+  criteria: Partial<Omit<User, "type">>
+): User[];
+export function filterPersons(
+  persons: Person[],
+  personType: "admin",
+  criteria: Partial<Omit<Admin, "type">>
+): Admin[];
+export function filterPersons(
+  persons: Person[],
   personType: string,
-  criteria: { age: number }
+  criteria: Partial<Omit<Person, "type">>
 ): Person[] {
   return persons
     .filter((person) => person.type === personType)
